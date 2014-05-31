97bb4667b3b2d85288ca5e26a01581faa4a239f1
Julius Cebreros <me@juliuscebreros.com>
2014-05-31 18:28:21 +0800

    tap tap

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,8 @@
 
   function Game() {
     this.player = null;
+    this.force = 0;
+    this.gravity = 0.6;
   }
 
   Game.prototype = {
@@ -23,24 +25,34 @@
     update: function () {
       var x, y, cx, cy, dx, dy, angle, scale;
 
-      x = this.input.position.x;
-      y = this.input.position.y;
-      cx = this.world.centerX;
-      cy = this.world.centerY;
-
-      angle = Math.atan2(y - cy, x - cx) * (180 / Math.PI);
-      this.player.angle = angle;
-
-      dx = x - cx;
-      dy = y - cy;
-      scale = Math.sqrt(dx * dx + dy * dy) / 100;
-
-      this.player.scale.x = scale * 0.6;
-      this.player.scale.y = scale * 0.6;
+      // x = this.input.position.x;
+      // y = this.input.position.y;
+      // cx = this.world.centerX;
+      // cy = this.world.centerY;
+      //
+      // angle = Math.atan2(y - cy, x - cx) * (180 / Math.PI);
+      // this.player.angle = angle;
+      //
+      // dx = x - cx;
+      // dy = y - cy;
+      // scale = Math.sqrt(dx * dx + dy * dy) / 100;
+      //
+      // this.player.scale.x = scale * 0.6;
+      // this.player.scale.y = scale * 0.6;
+      var accel = this.gravity + this.force;
+      this.player.y += accel;
+      
+      if(this.force < 0){
+        this.force += this.gravity;
+      }else{
+        this.force = 0.6;
+      }
     },
 
     onInputDown: function () {
-      this.game.state.start('menu');
+      // this.game.state.start('menu');
+      // this.player.y = this.world.centerY;
+      this.force = -5;
     }
 
   };
