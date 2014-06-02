5e43b0cefa708e05f27a9f9680ebbfe03aff67ab
Julius Cebreros <me@juliuscebreros.com>
2014-06-02 20:09:04 +0800

    formatting

--- src/js/game.js
+++ src/js/game.js
@@ -32,29 +32,10 @@
     },
 
     update: function () {
-      // var x, y, cx, cy, dx, dy, angle, scale;
-
-      // x = this.input.position.x;
-      // y = this.input.position.y;
-      // cx = this.world.centerX;
-      // cy = this.world.centerY;
-      //
-      // angle = Math.atan2(y - cy, x - cx) * (180 / Math.PI);
-      // this.player.angle = angle;
-
-      //
-      // dx = x - cx;
-      // dy = y - cy;
-      // scale = Math.sqrt(dx * dx + dy * dy) / 100;
-      //
-      // this.player.scale.x = scale * 0.6;
-      // this.player.scale.y = scale * 0.6;
-
       this.force += this.gravity;
       if(this.force >= 5){//terminal velocity
         this.force = 5;
       }
-
 
       this.accel = this.force;
       var angle = Math.atan2((this.player.y + this.accel) - this.player.y, this.player.x - (this.player.x-10)) * (180/Math.PI);
