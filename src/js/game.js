47c15eda6afd91f4381ab3db69df8a8dda752677
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2014-07-08 16:19:22 +0800

    Add pipes

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,7 @@
 
   function Game() {
     this.player = null;
+    this.pipes = null;
     this.force = 0;
     this.gravity = 0.6;
     this.accel = 0;
@@ -14,48 +15,88 @@
       var x = this.game.width / 2
         , y = this.game.height / 2;
 
-      this.player = this.add.sprite(x, y, 'player');
+      this.game.physics.startSystem(Phaser.Physics.Arcade);
+      this.game.physics.arcade.setBoundsToWorld();
+      console.log('bounds', this.game.physics);
+
+      this.player = this.game.add.sprite(x, y, 'player');
       this.player.animations.add('walk', [0,1,2], 6, true);
+      this.game.physics.arcade.enable(this.player);
+      this.player.body.gravity.y = 1000;
+      this.player.body.allowRotation = true;
+
       console.log(this.player);
 
       this.player.animations.play('walk', 50, true);
       this.player.anchor.setTo(0.5, 0.5);
-      this.input.onHold.add(this.onInputDown, this);
-      this.input.onTap.add(function(){
-        console.log('On tap');
-      }, this);
-      this.input.onDown.add(function(){
-        console.log('On down');
-      }, this);
+
+      this.pipes = this.game.add.group();
+      this.pipes.createMultiple(20, 'pipe');
+      console.log('pipes:', this.pipes);
+
       console.log(this.input);
       this.input.onUp.add(this.onInputUp, this);
+
+      this.timer = this.game.time.events.loop(1500, this.addRowOfPipes, this);
     },
 
     update: function () {
-      this.force += this.gravity;
-      if(this.force >= 5){//terminal velocity
-        this.force = 5;
-      }
+      this.player.body.angularVelocity = this.player.body.velocity.y / 2;
+    },
 
-      this.accel = this.force;
-      var angle = Math.atan2((this.player.y + this.accel) - this.player.y, this.player.x - (this.player.x-10)) * (180/Math.PI);
-      this.player.angle = angle;
-
-      this.player.y += this.accel;
+    render: function(){
+      this.game.debug.body(this.player);
     },
 
     onInputUp: function(){
-      this.force = -10;
+      // Add a vertical velocity to the bird
+      this.player.body.velocity.y = -350;
       console.log('end');
     },
 
-    onInputDown: function () {
-      // this.game.state.start('menu');
-      // this.player.y = this.world.centerY;
-      // this.force = -10;
-      console.log('start');
-    }
+    setPipeProperty: function(pipe){
+      this.game.physics.arcade.enable(pipe);
+      pipe.height = 60;
+      pipe.body.velocity.x = -200;
+      pipe.checkWorldBounds = true;
+      pipe.outOfBoundsKill = true;
+    },
 
+    addPipeEnd: function(x, y, img){
+      var pipe = this.game.add.sprite(x, y, img);
+      console.log('addPipeEnd', pipe);
+      this.setPipeProperty(pipe);
+    },
+
+    addOnePipe: function(x, y){
+      var pipe = this.pipes.getFirstDead();
+      pipe.reset(x, y);
+      this.setPipeProperty(pipe);
+      pipe.events.onOutOfBounds.add(function(){
+        console.log('i am dead!');
+      });
+    },
+
+    addRowOfPipes: function(){
+      var hole = Math.floor(Math.random()*5)+1;
+
+      for (var i = 0; i < 8; i++){
+        var x = 400,
+            y = i*60+10;
+
+        if(i === hole+1){
+          this.addPipeEnd(x, y, 'pipedown');
+        }
+
+        if(i === hole-1){
+          this.addPipeEnd(x, y, 'pipeup');
+        }
+
+        if(i !== hole && i !== hole +1){
+          this.addOnePipe(x, y);
+        }
+      }
+    }
   };
 
   window['flaptactics'] = window['flaptactics'] || {};
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -15,6 +15,9 @@
       this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
       this.load.setPreloadSprite(this.asset);
       this.load.spritesheet('player', 'assets/bird.png', 34, 96/4, 4);
+      this.load.image('pipe', 'assets/pipe.png');
+      this.load.image('pipeup', 'assets/pipe-down.png');
+      this.load.image('pipedown', 'assets/pipe-up.png');
       this.load.bitmapFont('minecraftia', 'assets/minecraftia.png', 'assets/minecraftia.xml');
     },
 
