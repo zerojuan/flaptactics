f496d5e1f4e7bff762004d0cf68e0d8399f8d8c3
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2014-07-11 11:24:10 +0800

    [Fix] scoring

--- src/js/game.js
+++ src/js/game.js
@@ -6,8 +6,12 @@
     this.pipes = null;
     this.pipesUp = null;
     this.pipesDown = null;
+    this.rectangles = null;
     this.land = null;
     this.sky = null;
+    this.subtotal = 0;
+    this.score = null;
+    this.medal = null;
     this.scoreboard = null;
     this.replay = null;
     this.force = 0;
@@ -23,6 +27,7 @@
         , y = this.game.height / 2;
       console.log('this game:', this.game);
 
+      this.subtotal = 0;
       this.game.stage.backgroundColor = '#4EC0CA';
 
       // enable physics for game
@@ -47,11 +52,16 @@
       this.pipesDown.createMultiple(20, 'pipedown');
       console.log('pipes:', this.pipes);
 
+      // points
+      this.rectangles = this.game.add.group();
+      this.rectangles.createMultiple(20, 'pipe');
+
       // player
       this.player = this.game.add.sprite(x, y, 'player');
       this.player.animations.add('walk', [0,1,2], 6, true);
       this.game.physics.arcade.enable(this.player);
       this.player.body.gravity.y = 1000;
+      this.player.body.setSize(1, 20, 0, 0);
       this.player.body.allowRotation = true;
       this.game.physics.arcade.enable(this.player);
       this.player.checkWorldBounds = true;
@@ -72,6 +82,7 @@
     update: function () {
       this.player.body.angularVelocity = this.player.body.velocity.y / 2;
       this.game.physics.arcade.collide(this.pipes, this.player, this.collisionHandler, null, this);
+      this.game.physics.arcade.overlap(this.rectangles, this.player, this.scorer, null, this);
     },
 
     restart: function(){
@@ -81,6 +92,9 @@
 
     render: function(){
       this.game.debug.body(this.player);
+      this.rectangles.forEach(function(rect){
+        this.game.debug.spriteBounds(rect);
+      }, this);
     },
 
     onInputUp: function(){
@@ -118,11 +132,21 @@
       });
     },
 
+    addPointPipe: function(x, y){
+      var pipe = this.rectangles.getFirstDead();
+      pipe.reset(x, y);
+      console.log(x, pipe)
+      this.setPipeProperty(pipe);
+      pipe.height = 150;
+      pipe.width = 1;
+      pipe.events.onOutOfBounds.add(function(){});
+    },
+
     addRowOfPipes: function(){
       var hole = Math.floor(Math.random()*5)+1;
 
       for (var i = 0; i < 8; i++){
-        var x = this.game.width,
+        var x = this.game.width - 60,
             y = i*60;
 
         if(i === hole+1){
@@ -130,6 +154,8 @@
         }
 
         if(i === hole-1){
+          // add collision for point
+          this.addPointPipe(x + 60, y + 50);
           this.addPipeEnd(x, y + 35, 'pipesUp');
         }
 
@@ -146,13 +172,62 @@
       this.pipes.setAll('body.velocity.x', 0, true);
       this.pipesUp.setAll('body.velocity.x', 0, true);
       this.pipesDown.setAll('body.velocity.x', 0, true);
+      this.rectangles.setAll('body.velocity.x', 0, true);
 
-      this.scoreboard = this.game.add.sprite(this.game.width / 3, 0, 'scoreboard');
-      this.replay = this.game.add.sprite(this.game.width / 3, this.game.height / 2, 'replay');
+      this.scoreboard = this.game.add.sprite(this.game.width / 3, this.game.height / 6, 'scoreboard');
+
+      this.score = this.game.add.group();
+      this.displayScore();
+      this.displayMedal();
+      this.replay = this.game.add.sprite(this.game.width / 1.9, this.game.height - 200, 'replay');
       this.replay.inputEnabled = true;
       this.replay.events.onInputDown.add(this.restart, this);
-      console.log(this.player);
+
       console.log('killed');
+    },
+
+    scorer: function(obj1, obj2){
+      console.log('score?', this.subtotal, obj1.getBounds(), obj2.getBounds());
+      if(Phaser.Rectangle.intersects(obj1.getBounds(), obj2.getBounds())){
+        console.log('intersects!!!!!!!!!');
+        this.subtotal++;
+      }
+    },
+
+    displayScore: function(){
+      var splits = this.subtotal.toString().split(''),
+          divideX = 1.50,
+          divideY = 2.6;
+
+      // console.log('subtotal', this.subtotal, 'splits', splits);
+      for(var i = splits.length - 1; i > -1; i--){
+        divideX = divideX + 0.05;
+        // console.log(divideX, divideY, splits[i]);
+        this.score.create(this.game.width / divideX, this.game.height / divideY, splits[i]);
+      }
+    },
+
+    displayMedal: function(){
+      var x = this.game.width / 2.6,
+          y = this.game.height / 2.5;
+
+      switch(true){
+        case this.subtotal < 50:
+          // console.log('bronze', this.subtotal);
+          this.medal = this.game.add.sprite(x, y, 'bronze');
+          break;
+        case this.subtotal < 150:
+          // console.log('silver', this.subtotal);
+          this.medal = this.game.add.sprite(x, y, 'silver');
+          break;
+        case this.subtotal > 149:
+          // console.log('gold', this.subtotal);
+          this.medal = this.game.add.sprite(x, y, 'gold');
+          break;
+        default:
+          this.medal = this.game.add.sprite(x, y, 'bronze');
+          break;
+      }
     }
   };
 
